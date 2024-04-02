a7d1c150bf6abb645be80696d768ad8721df459d
SUNGKYU KIM <seooonggyu@gmail.com>
2024-04-02 22:48:36 +0900

    hello.cpp

--- Lab5/hello.cpp
+++ Lab5/hello.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
-    cout << "Hello World" << endl;
+    string s1, s2;
+
+    cin >> s1;
+    cout << s1 << endl;
+
+    cin.ignore();
+
+    getline(cin, s2);
+    cout << s2 ;
+
+    //cout << "Hello World" << endl;
     return 0;
 }
