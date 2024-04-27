7a12735fc4db1761d0ef7e833c2605a0ab151217
SUNGKYU KIM <seooonggyu@gmail.com>
2024-04-27 17:19:56 +0900

    DS023.cpp

---
+++ Lab7/DS023.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class Prod{
+    private :
+        string name;
+        int price;
+        int discount;
+        string amount;
+        string manuf;
+        int size;
+    public :
+        int addProd(int s){
+            cin.ignore();
+            getline(cin, name);
+            cin >> price >> discount >> amount >> manuf;
+            s++;
+            return s;
+        }
+        void listProd(){
+            if(discount!=0)
+                cout << discountedPrice() << " (-" << discount << "%) " << name << " " << amount << " " << manuf <<endl;
+            else
+                cout << discountedPrice() << " (" << discount << "%) " << name << " " << amount << " " << manuf <<endl;
+        }
+        int discountedPrice(){
+            return price - (price*discount/100);
+        }
+};
+
+int main(){
+    int size = 10;
+    Prod* item = new Prod[size];
+
+    int s=0;
+    int input=0;
+    
+    while(1){
+        cout << "1. Add 2. List 3. Quit > ";
+        cin >> input;
+        if(input == 1) s=item[s].addProd(s);
+        else if(input == 2){
+            for(int i=0; i<s; i++){
+                item[i].listProd();
+            }
+        }else break;
+    }
+
+    delete[] item;
+    
+    return 0;
+}
